88b5c5d179ebebbdff14523457f060000ebcd926
lubov-nefed <love.nefed@gmail.com>
2023-11-30 16:19:43 +0300

    feat: add server

--- core.php
+++ core.php
@@ -7,19 +7,55 @@ class Page {
 
   public function __construct($title, $body) {
     $this->title = $title;
-    $this->bodt = $body;
+    $this->body = $body;
   }
 
   $articles = [
-    new Page("About","
-    <h4 class='app__heading' style='background-color: #8a00d4; color: #ffff;'>About</h4>");
+   /*  new Page("About","
+    <h4 id="about" class='app__heading' style='background-color: #8a00d4; color: #ffff;'>About</h4>");
     new Page("Blog","
-    <h4 class='app__heading' style='background-color: #d527b7; color: #ffff;'>Blog</h4>");
+    <h4 id="blog" class='app__heading' style='background-color: #d527b7; color: #ffff;'>Blog</h4>");
     new Page("Pricing","
-    <h4 class='app__heading' style='background-color: #f9c46b; color: #ffff;'>Pricing</h4>");
+    <h4 id="pricing" class='app__heading' style='background-color: #f9c46b; color: #ffff;'>Pricing</h4>");
     new Page("Support","
-    <h4 class='app__heading' style='background-color: #34ee78; color: #ffff;'>Support</h4>");
-  ]
+    <h4 id="support" class='app__heading' style='background-color: #34ee78; color: #ffff;'>Support</h4>"); */
+    new Page("Article 1", "<p>asdas 1</p> <a href='/Main' class='link link_internal'>Return to main page</a>"),
+    new Page("Article 2", "<p>asdas 2</p> <a href='/Main' class='link link_internal'>Return to main page</a>"),
+    new Page("Article 3", "<p>asdas 3</p> <a href='/Main' class='link link_internal'>Return to main page</a>"),
+    new Page("Article 4", "<p>asdas 4</p> <a href='/Main' class='link link_internal'>Return to main page</a>")
+  ];
+
+  if (isset($_GET["page"])) {//"page" is part of request ?page=articles&id=1
+    $page = $_GET["page"];//значение будет article или main
+  } else {
+    $page = "404";
+  }
+  if (isset($_GET["id"])) {//число получаемое из массива props в функции LinkClick, получаемое в свою очередь из html-разметки по ссылкам anchor
+    $id = $_GET["id"];
+  } else {
+    $id = 0;
+  }
+  $response = new Page("404", "<p>Page not found</p> <a href='/' class='link link_internal'>Return to main page</a>");
+
+  switch($page) {
+    case "main":
+      $response = new Page("Main", "
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, iste</p>
+      <a href='/Articles/1' class='link_internal'>Article 1</a>
+      <a href='/Articles/2' class='link_internal'>Article 2</a>
+      <a href='/Articles/3' class='link_internal'>Article 3</a>
+      <a href='/Articles/4' class='link_internal'>Article 4</a>
+      ")
+    case "articles":
+      if ($id > 0) {
+        if (isset($articles[$id - 1])) {
+          $response = $articles[$id - 1];
+        }
+      }
+      break;
+  }
 }
 
+
+die(json_encode($response));
 ?>
---
+++ main.js
@@ -0,0 +1,75 @@
+let links = null;
+
+let loaded = true;
+
+let data = {
+  title: "",
+  body: "",
+  link: ""
+};
+
+let page = {
+  title: document.getElementById("title"),
+  body: document.getElementById("body")
+};
+/* 
+function GetData(response, link) {
+  data = {
+    title: response.title,
+    body: response.body,
+    link: link
+  }
+  UpdatePage();//3
+} */
+
+function SendRequest(query, link) {
+  console.log('executed');
+  console.log(link);
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "spa/core.php" + query, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+    loaded = true;
+    if (xhr.status == 200) { 
+      console.log(JSON.parse(xhr.responseText));
+     /*  GetData(JSON.parse(xhr.responseText), link) *///2
+    }
+  }
+};
+
+const LinkClick = (href) => (event) => {
+  event.preventDefault();
+  let props = href.split("/");
+  console.log(props);// ['', 'Articles', '1'] or  ['', 'Main']
+  switch(props[1])// при нажатии на ссылку на главной странице props = 'Articles'
+  {
+    case "Main":
+      console.log(href);
+      SendRequest("?page=main", href); //Отправляем запрос на сервер
+      break;
+
+    case "Articles"://Проверяем валидность идентификатора и тоже отправляем запрос
+      if(props.length == 3 && !isNaN(props[2]) && Number(props[2]) > 0) //изменить под свои условия
+      {
+        SendRequest("?page=articles&id=" + props[2], href);//?page=articles&id=pricing изменить под свои условия
+      }
+      break;
+  }
+  
+}
+
+function InitLinks() {
+  links = document.querySelectorAll(".link_internal");
+  links.forEach(link => link.addEventListener("click", LinkClick(link.getAttribute("href"))));
+}
+
+/* function UpdatePage() {
+  page.title.innerText = data.title;
+  page.body.innerHTML = data.body;
+
+  document.title = data.title;
+  window.history.pushState(data.body, data.title, "/spa" + data.link);
+  InitLinks();// 4
+} */
+
+InitLinks();
